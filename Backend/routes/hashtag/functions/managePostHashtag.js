36228c76f718e60a89ecc6877951700ee19b03a8
Hemanth <33123731+Hemanth759@users.noreply.github.com>
2020-04-11 15:28:30 +0530

    Removed additional errors

--- Backend/routes/hashtag/functions/managePostHashtag.js
+++ Backend/routes/hashtag/functions/managePostHashtag.js
@@ -3,7 +3,7 @@
 const { hashtagsValidation } = require("../hashtagValidations");
 
 const addPostToHashtag = require("./addPostToHashtag");
-const removePostFromHashtag = require("../../post/functions/removePostFromHashtag");
+const removePostFromHashtag = require("./removePostFromHashtag");
 const deleteHashtag = require("./deleteHashtag");
 
 // adding post to hashtag immediately after creation
@@ -34,7 +34,11 @@ module.exports = async (req, res) => {
             const old_hashtags = req.oldPostHM.hashtags;
             for (let i = 0; i < old_hashtags.length; i++) {
                 // removing post id from hashtag
-                let hashtag = await removePostFromHashtag(req, res, old_hashtags[i]);
+                let hashtag = await removePostFromHashtag(
+                    req,
+                    res,
+                    old_hashtags[i]
+                );
 
                 // check if hashtag has 0 posts
                 if (hashtag) {
@@ -61,7 +65,6 @@ module.exports = async (req, res) => {
                 .json({ message: "Post deleted succesfully" });
         }
         return res.status(200).json(req.post.toMap());
-
     } catch (error) {
         console.log(error);
         return res.status(500).json({ message: "Internal server error" });
