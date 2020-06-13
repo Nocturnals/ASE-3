378cc5730d70b8f234a6ce5fe63264830ac32775
Hemanth Vanam <33123731+Hemanth759@users.noreply.github.com>
2020-06-13 15:43:58 +0530

    Added the error message showing to registerpage

--- pet_app/lib/views/authentication/login/loginScreen.dart
+++ pet_app/lib/views/authentication/login/loginScreen.dart
@@ -2,14 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
 import 'package:flutter_redux/flutter_redux.dart';
-import 'package:pet_app/models/loadingStatus.dart';
 import 'package:pet_app/redux/state.dart';
-import 'package:pet_app/views/authentication/login/message.dart';
 import 'package:redux/redux.dart';
 import 'package:responsive_builder/responsive_builder.dart' hide WidgetBuilder;
 
 import 'package:pet_app/views/authentication/login/mobileView.dart';
 import 'package:pet_app/views/authentication/login/desktopTabletView.dart';
+import 'package:pet_app/views/notifications/notificationChecker.dart';
+import 'package:pet_app/views/notifications/notifications.dart';
 
 import 'package:pet_app/widgets/cButtons.dart';
 import 'package:pet_app/widgets/BezierContainer.dart';
@@ -83,55 +83,14 @@ class LoginScreen extends StatelessWidget {
           },
         );
       },
-      child: StoreConnector<AppState, MessageViewModel>(
-        converter: (Store<AppState> store) => MessageViewModel.create(store),
-        builder: (BuildContext context, MessageViewModel messageViewModel) {
-          // if message exists clear is after some time
-          if (messageViewModel.state.loadingStatus == LoadingStatus.error) {
-            Future.delayed(const Duration(milliseconds: 50), () {
-              showDialog(
-                  context: context,
-                  builder: (_) {
-                    return AlertDialog(
-                      title: Center(
-                        child: Text('Error'),
-                      ),
-                      content: Text(messageViewModel.state.errorMessage),
-                      actions: <Widget>[
-                        FlatButton(
-                          onPressed: () {
-                            messageViewModel.resetMessage();
-                            Navigator.of(context, rootNavigator: true).pop('dialog');
-                          },
-                          child: Text('Retry'),
-                        )
-                      ],
-                    );
-                  });
-            });
-          } else if (messageViewModel.state.loadingStatus == LoadingStatus.showMessage) {
-            Future.delayed(const Duration(milliseconds: 50), () {
-              showDialog(
-                  context: context,
-                  builder: (_) {
-                    return AlertDialog(
-                      title: Center(
-                        child: Text('Error'),
-                      ),
-                      content: Text(messageViewModel.state.notifyMessage),
-                      actions: <Widget>[
-                        FlatButton(
-                          onPressed: () {
-                            messageViewModel.resetMessage();
-                            Navigator.of(context, rootNavigator: true).pop('dialog');
-                          },
-                          child: Text('Retry'),
-                        )
-                      ],
-                    );
-                  });
-            });
-          }
+      child: StoreConnector<AppState, NotificationViewModel>(
+        converter: (Store<AppState> store) =>
+            NotificationViewModel.create(store),
+        builder: (BuildContext context,
+            NotificationViewModel notificationViewModel) {
+          // checks for notifications if any shows
+          notificationChecker(
+              notificationViewModel: notificationViewModel, context: context);
 
           // return the sceen
           return Scaffold(
--- pet_app/lib/views/notifications/notifications.dart
+++ pet_app/lib/views/notifications/notifications.dart
@@ -7,25 +7,25 @@ import 'package:pet_app/redux/auth/authActions.dart';
 import 'package:pet_app/redux/auth/authState.dart' show AuthState;
 import 'package:redux_thunk/redux_thunk.dart';
 
-class MessageViewModel {
+class NotificationViewModel {
   final AuthState state;
-  final Function resetMessage;
+  final Function resetNotification;
 
-  MessageViewModel({@required this.state, @required this.resetMessage});
+  NotificationViewModel({@required this.state, @required this.resetNotification});
 
-  factory MessageViewModel.create(Store<AppState> store) {
+  factory NotificationViewModel.create(Store<AppState> store) {
     resetMessage() {
-      store.dispatch(_resetMessage());
+      store.dispatch(_resetNotification());
     }
 
-    return MessageViewModel(
+    return NotificationViewModel(
       state: store.state.authState,
-      resetMessage: resetMessage,
+      resetNotification: resetMessage,
     );
   }
 }
 
-ThunkAction _resetMessage() {
+ThunkAction _resetNotification() {
   return (Store store) async {
     Future(() async {
       // reset the message after some time
